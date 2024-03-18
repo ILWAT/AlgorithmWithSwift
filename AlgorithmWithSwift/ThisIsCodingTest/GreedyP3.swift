bb231766723f99bdcdf89806f59b0d8917a85e9e
J.H. Moon <moonjh587@naver.com>
2024-03-18 20:53:27 +0900

    이것이 코딩테스트다 예제

---
+++ AlgorithmWithSwift/ThisIsCodingTest/GreedyP3.swift
@@ -0,0 +1,30 @@
+//
+//  GreedyP3.swift
+//  AlgorithmWithSwift
+//
+//  Created by 문정호 on 3/18/24.
+//
+
+import Foundation
+
+func greedyP3() {
+    //n: 행의 갯수 / m: 열의 갯수
+    var n: Int = 0
+    var m: Int = 0
+    var rowMin = 10001
+    var totalMax = 0
+
+    let firstInput = readLine()!.split(separator: " ").map({ Int($0)! })
+
+    n = firstInput[0]
+    m = firstInput[1]
+
+    for _ in 0..<n {
+        let rowValue = readLine()!.split(separator: " ").map({ Int($0)! })
+        rowMin = rowValue.min()!
+        totalMax = max(totalMax, rowMin)
+    }
+
+    print(totalMax)
+}
+
